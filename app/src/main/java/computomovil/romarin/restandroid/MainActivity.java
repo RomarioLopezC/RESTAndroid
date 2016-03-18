69c38113677f6768691fecb2d4f34e368348fdaa
Romario Lopez C <romariolopezc@gmail.com>
2016-03-17 21:53:15 -0600

    Initial commit

---
+++ app/src/main/java/computomovil/romarin/restandroid/MainActivity.java
@@ -0,0 +1,17 @@
+package computomovil.romarin.restandroid;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        FetchDataTask fetchDataTask = new FetchDataTask((TextView) findViewById(R.id.textViewJson));
+        fetchDataTask.execute("Start");
+    }
+}
