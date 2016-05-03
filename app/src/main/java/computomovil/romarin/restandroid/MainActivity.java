9e2defbf5615c1a7c4ded318a9efe9138a5978a3
Romario Lopez C <romariolopezc@gmail.com>
2016-05-03 11:20:14 -0500

    Para el json

--- app/src/main/java/computomovil/romarin/restandroid/MainActivity.java
+++ app/src/main/java/computomovil/romarin/restandroid/MainActivity.java
@@ -11,7 +11,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        FetchDataTask fetchDataTask = new FetchDataTask((TextView) findViewById(R.id.textViewJson));
-        fetchDataTask.execute("Start");
+
     }
 }
